06ab4bdc1b0a8e1c22a4fc2a6ea8eb3536cd6ff5
Omar-jabra <129175570+Omar-Tood@users.noreply.github.com>
2024-02-08 00:21:27 +0300

    updates our routes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,9 +4,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello World
-      </p>
-      <Button>Click Me?</Button>
+      
+  This a projected Route
     </div>
   );
 }
--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div className="text-3xl text-indigo-500 p-2 font-bold">Omar jibril! Wan Kusalamey</div>
-  )
-}
-
-export default page
